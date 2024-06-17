fe3d60e8164c933bb43b1e288ce74fb4827eb190
Iqbal Ramadan <78259947+balramadan@users.noreply.github.com>
2024-06-17 14:59:43 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rnjqyqiohdhnlcidizdw.supabase.co";
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuanF5cWlvaGRobmxjaWRpemR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxOTk4NjIsImV4cCI6MjAyOTc3NTg2Mn0.NKugMvDNBhJJSinMRg59avkbxPE413F-6H1n-DgNlOY';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 export default supabase;
